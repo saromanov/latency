3698f1342ae94e434e07a85d62e5d45115955673
Sergey <xxsmotur@gmail.com>
2022-11-01 23:53:21 +0500

    Update on initialization

--- internal/latency/config.go
+++ internal/latency/config.go
@@ -4,7 +4,9 @@ import "time"
 
 // Config defines configuration for latency project
 type Config struct {
-	Address string    `yaml:"address"`
-	Port    int       `yaml:"port"`
-	Latency time.Time `yaml:"latency"`
+	Address     string    `yaml:"address"`
+	DestAddress string    `yaml:"destAddress"`
+	Port        int       `yaml:"port"`
+	Latency     time.Time `yaml:"latency"`
+	QueueSize   int       `yaml:"queueSize"`
 }
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main(){
-	
+import (
+	"os"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/saromanov/latency/internal/latency"
+)
+
+// parseArgs provides parsing of argumenys
+func parseArgs(args []string) (latency.Config, error) {
+	var app = kingpin.New("latency", "Proxy for simulating network latency")
+	var (
+		address = app.Flag("address", "Address of the host").Default("localhost").String()
+		destAddress = app.Flag("destAddress", "Address of the destanation host").String()
+		port = app.Flag("port", "Port").Default("8080").Int()
+	)
+
+	if _, err := app.Parse(args); err != nil {
+		return latency.Config{}, err
+	}
+
+	return latency.Config{
+		Address: *address,
+		Port: *port,
+		DestAddress: *destAddress,
+	}, nil
+}
+func main() {
+	data, err := parseArgs(os.Args[1:])
+	if err != nil {
+		panic(err)
+	}
+
+	l := latency.New(data)
+
+	if err := l.Init(); err != nil {
+		panic(err)
+	}
 }
