d4cb2de38f2cd21d540b9daf349c9a1033b52542
Sergey <xxsmotur@gmail.com>
2022-11-06 14:55:19 +0500

    Adding loop on Connection

--- internal/latency/connection.go
+++ internal/latency/connection.go
@@ -3,20 +3,36 @@ package latency
 import (
 	"context"
 	"net"
+
+	"github.com/sirupsen/logrus"
 )
 
 type Connection struct {
 	conn, writeConn *net.TCPConn
 	bufferSize      int
+	done            chan error
 }
 
 func NewConnection(conn *net.TCPConn) *Connection {
 	return &Connection{
 		conn: conn,
+		done: make(chan error),
 	}
 }
 
 func (c *Connection) Start(ctx context.Context) error {
+	log := logrus.WithContext(ctx)
+	go c.readFromBuffer(ctx)
+	for {
+		select {
+		case err := <-c.done:
+			log.WithError(err).Error("error on done method")
+			return err
+		}
+	}
+}
+
+func (c *Connection) readFromBuffer(ctx context.Context) error {
 	buffer := make([]byte, c.bufferSize)
 	for {
 		data, err := c.conn.Read(buffer)
